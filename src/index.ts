4817571e552b6e92cd8af1dd7b1669184fcf1355
Christian Svenkerud <csvenke@gmail.com>
2020-04-11 15:29:29 +0200

    feat: add  bail functionality to not composer

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,47 @@
 type RuleFunction = (arg: any) => boolean;
 
 type RuleComposerConfig = {
-  getResults: (rules: RuleFunction[], arg: any) => boolean[];
-  getResult: (results: boolean[]) => boolean;
+  resolveRules: (rules: RuleFunction[], arg: any) => boolean[];
+  resolveResult: (results: boolean[]) => boolean;
 };
 
 const makeRuleComposer = (config: RuleComposerConfig) => (
   ...rules: RuleFunction[]
 ) => (arg: any) => {
-  const { getResults, getResult } = config;
-  const results = getResults(rules, arg);
-  const result = getResult(results);
+  const { resolveRules, resolveResult } = config;
+  const results = resolveRules(rules, arg);
+  const result = resolveResult(results);
   return result;
 };
 
-const getResults = (rules: RuleFunction[], arg: any) =>
+const resolveRulesWithBail = (rules: RuleFunction[], arg: any) => {
+  let results = [];
+  for (let rule of rules) {
+    const result = rule(arg);
+    results.push(result);
+    if (!result) {
+      break;
+    }
+  }
+  return results;
+};
+
+const resolveAllRules = (rules: RuleFunction[], arg: any) =>
   rules.map(rule => rule(arg));
 
 const and = makeRuleComposer({
-  getResults: (rules, arg) => {
-    let results = [];
-    for (let rule of rules) {
-      const result = rule(arg);
-      results.push(result);
-      if (!result) {
-        break;
-      }
-    }
-    return results;
-  },
-  getResult: results => results.every(result => result)
-});
-
-const or = makeRuleComposer({
-  getResults,
-  getResult: results => results.some(result => result)
+  resolveRules: resolveRulesWithBail,
+  resolveResult: results => results.every(result => result)
 });
 
 const not = makeRuleComposer({
-  getResults,
-  getResult: results => results.every(result => !result)
+  resolveRules: resolveRulesWithBail,
+  resolveResult: results => results.every(result => !result)
+});
+
+const or = makeRuleComposer({
+  resolveRules: resolveAllRules,
+  resolveResult: results => results.some(result => result)
 });
 
 export { and, or, not };
