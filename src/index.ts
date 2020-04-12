20c469ca5c2042e0ceb6f378b33a93ae257c13ff
Christian Svenkerud <csvenke@gmail.com>
2020-04-13 01:13:27 +0200

    feat: improve performance for or composer by bailing on first true

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,16 @@ function makeRuleComposer(handler: Handler) {
   return <T = any>(...rules: Rule<T>[]): Rule<T> => arg => handler(rules, arg);
 }
 
-function resolveRules(rules: Rule<any>[], arg: any) {
+function resolveRules(rules: Rule<any>[], arg: any, breakOn = false) {
   let results = [];
   for (let rule of rules) {
     const result = rule(arg);
     results.push(result);
-    if (!result) {
+    if (result === breakOn) {
       break;
     }
   }
   return results;
-}
-
-function resolveAllRules(rules: Rule<any>[], arg: any) {
-  return rules.map(rule => rule(arg));
 }
 
 function isTrue(result: boolean) {
@@ -36,7 +32,7 @@ const and = makeRuleComposer((rules, arg) => {
 });
 
 const or = makeRuleComposer((rules, arg) => {
-  const results = resolveAllRules(rules, arg);
+  const results = resolveRules(rules, arg, true);
   return results.some(isTrue);
 });
 
